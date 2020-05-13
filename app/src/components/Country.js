f4acc633b4f16e4394fbe13f5bfcd9951acb1f26
toricruzmendiola <mtoricruz@gmail.com>
2020-05-13 16:33:19 -0700

    mvp met for styling. will do more styling when i get home later tonight

--- app/src/components/Country.js
+++ app/src/components/Country.js
@@ -1,7 +1,9 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 
-import { fetchCountry } from '../actions/countryAction'
+import { fetchCountry } from '../actions/countryAction';
+
+import './Country.css'
 
 const Country = ({
     isFetchingCountry,
@@ -19,17 +21,27 @@ const Country = ({
 
     return (
         <main>
-            <h1>USA Covid-19 Info</h1>
+            <div className='app-title-container'>
+                <div className='app-title-div'>
+                    <h1>USA Covid-19 Info</h1>
+                </div>
+            </div>
             {isFetchingCountry && <h3>Fetching Data...</h3>}
             {/* list of stats */}
             {!isFetchingCountry && (
                 <div>
-                    <p>New Confirmed Cases: {NewConfirmed}</p>
-                    <p>New Death Cases: {NewDeaths}</p>
-                    <p>Newly Recovered Cases: {NewRecovered}</p>
-                    <p>Total Confirmed Cases: {TotalConfirmed}</p>
-                    <p>Total Death Cases: {TotalDeaths}</p>
-                    <p>Total Recovered Cases: {TotalRecovered}</p>
+                    <div className='new-cases'>
+                        <h2>New Cases</h2>
+                        <p>New Confirmed Cases: {NewConfirmed}</p>
+                        <p>New Death Cases: {NewDeaths}</p>
+                        <p>Newly Recovered Cases: {NewRecovered}</p>
+                    </div>
+                    <div className='total-cases'>
+                        <h2>Total Cases</h2>
+                        <p>Total Confirmed Cases: {TotalConfirmed}</p>
+                        <p>Total Death Cases: {TotalDeaths}</p>
+                        <p>Total Recovered Cases: {TotalRecovered}</p>
+                    </div>
                 </div>
             )}
         </main>
